5e0a8c048c666f03b69aa87d41110cebe2b67506
Jhonny Pirela <jhosagid7@gmail.com>
2024-01-26 11:40:38 -0400

    Modify update-profile-information-form

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -36,54 +36,76 @@
                         x-bind:style="'background-image: url(\'' + photoPreview + '\');'">
                     </span>
                 </div>
-
-                <x-secondary-button class="mt-2 me-2" type="button" x-on:click.prevent="$refs.photo.click()">
-                    {{ __('Select A New Photo') }}
-                </x-secondary-button>
+                <x-adminlte-button class="btn-md" type="button" label="{{ __('Select A New Photo') }}"
+                    theme="outline-info" icon="fas fa-md fa-image" x-on:click.prevent="$refs.photo.click()" />
 
                 @if ($this->user->profile_photo_path)
-                    <x-secondary-button type="button" class="mt-2" wire:click="deleteProfilePhoto">
-                        {{ __('Remove Photo') }}
-                    </x-secondary-button>
+                    <x-adminlte-button class="btn-md" type="button" label="{{ __('Remove Photo') }}"
+                        theme="outline-danger" icon="fas fa-md fa-image" wire:click="deleteProfilePhoto" />
                 @endif
+
 
                 <x-input-error for="photo" class="mt-2" />
             </div>
         @endif
 
         <!-- Name -->
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}" />
-            <x-input id="name" type="text" class="mt-1 block w-full" wire:model="state.name" required
-                autocomplete="name" />
-            <x-input-error for="name" class="mt-2" />
+
+
+        <div class="row">
+            <x-adminlte-input id="name" name="Name" label="{{ __('Name') }}"
+                placeholder="{{ __('Name') }}" wire:model="state.name" required autocomplete="name"
+                fgroup-class="col-md-6">
+                <x-slot name="prependSlot">
+                    <div class="input-group-text">
+                        <i class="fas fa-user text-lightblue"></i>
+                    </div>
+                </x-slot>
+            </x-adminlte-input>
+            <x-input-error for="name" class="text-danger mt-2" />
         </div>
 
+
+
         <!-- Email -->
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="email" value="{{ __('Email') }}" />
-            <x-input id="email" type="email" class="mt-1 block w-full" wire:model="state.email" required
-                autocomplete="username" />
-            <x-input-error for="email" class="mt-2" />
+        <div class="row">
+            <x-adminlte-input id="email" name="Email" label="{{ __('Email') }}"
+                placeholder="{{ __('Email') }}" wire:model="state.email" required autocomplete="username"
+                fgroup-class="col-md-6">
+                <x-slot name="prependSlot">
+                    <div class="input-group-text">
+                        <i class="fas fa-envelope text-lightblue"></i>
+                    </div>
+                </x-slot>
+            </x-adminlte-input>
+            <x-input-error for="email" class="text-danger mt-2" />
+        </div>
 
-            @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) &&
-                    !$this->user->hasVerifiedEmail())
-                <p class="text-sm mt-2">
-                    {{ __('Your email address is unverified.') }}
 
-                    <button type="button"
-                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        wire:click.prevent="sendEmailVerification">
-                        {{ __('Click here to re-send the verification email.') }}
-                    </button>
+
+
+
+
+
+
+        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::emailVerification()) &&
+                !$this->user->hasVerifiedEmail())
+            <p class="text-sm mt-2">
+                {{ __('Your email address is unverified.') }}
+
+                <button type="button"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    wire:click.prevent="sendEmailVerification">
+                    {{ __('Click here to re-send the verification email.') }}
+                </button>
+            </p>
+
+            @if ($this->verificationLinkSent)
+                <p class="mt-2 font-medium text-sm text-green-600">
+                    {{ __('A new verification link has been sent to your email address.') }}
                 </p>
-
-                @if ($this->verificationLinkSent)
-                    <p class="mt-2 font-medium text-sm text-green-600">
-                        {{ __('A new verification link has been sent to your email address.') }}
-                    </p>
-                @endif
             @endif
+        @endif
         </div>
     </x-slot>
 
